08d5224bce9fe81331bc6e20fe475b6d24d8f07d
jayden <jeonghoochoe@gmail.com>
2024-04-28 13:37:10 +0100

    Update the SystemStarter. It can start the all backend server and keep running

--- src/main/java/com/grpcproject/SystemStarter.java
+++ src/main/java/com/grpcproject/SystemStarter.java
@@ -1,12 +1,26 @@
 package com.grpcproject;
 
-import com.grpcproject.smartfarm.SmartFarmSystemStarter;
+import com.grpcproject.smartfarm.EquipServer;
+import com.grpcproject.smartfarm.SensorServer;
+import com.grpcproject.smartfarm.SmartControlServer;
+import com.grpcproject.smartfarm.SensorAndEquipSimulator.*;
+
 
 import java.io.IOException;
 
 public class SystemStarter {
     public static void main(String[] args) throws IOException, InterruptedException {
-        SmartFarmSystemStarter smartFarmSystemStarter = new SmartFarmSystemStarter();
-        smartFarmSystemStarter.start();
+
+        SoilEnvironmentSimulator soilEnvironmentSimulator = new SoilEnvironmentSimulator();
+        EquipServer equipServer = new EquipServer();
+        SensorServer sensorServer = new SensorServer();
+        SmartControlServer smartControlServer = new SmartControlServer();
+
+        soilEnvironmentSimulator.start();
+        equipServer.start();
+        sensorServer.start();
+        smartControlServer.start();
+
+
     }
 }
