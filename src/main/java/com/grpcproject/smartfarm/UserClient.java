576dc4c57d298c9150e20e429ff02e6b83887d30
jayden <jeonghoochoe@gmail.com>
2024-04-28 04:41:01 +0100

    update userclient

--- src/main/java/com/grpcproject/smartfarm/UserClient.java
+++ src/main/java/com/grpcproject/smartfarm/UserClient.java
@@ -8,6 +8,11 @@ public class UserClient {
     private final ManagedChannel channel;
     private final UserMonitorAndControlServiceGrpc.UserMonitorAndControlServiceStub stub;
 
+    private long curTemp, curHumidity;
+    private long heaterRunningTimeLeft, sprinklerRunningTimeLeft;
+
+    private boolean isHeaterRunning, isSprinklerRunning;
+
 
     public UserClient() {
         ConsulFindUtil serverFind = new ConsulFindUtil(
@@ -19,6 +24,9 @@ public class UserClient {
                 .usePlaintext()
                 .build();
         this.stub = UserMonitorAndControlServiceGrpc.newStub(channel);
+
+        setHeaterRunning(false);
+        setSprinklerRunning(false);
     }
 
     public void refreshSensorData() {
@@ -28,7 +36,12 @@ public class UserClient {
 
                 String sensorName = refreshSensorDataRes.getSensorName();
                 int sensorData = refreshSensorDataRes.getSensorData();
-                System.out.println(sensorName + " : " + sensorData);
+
+                if(sensorName.equals("soil temp")){
+                    setCurTemp(sensorData);
+                }else if(sensorName.equals("soil humidity")){
+                    setCurHumidity(sensorData);
+                }
             }
 
             @Override
@@ -38,7 +51,8 @@ public class UserClient {
 
             @Override
             public void onCompleted() {
-                System.out.println("Smart controller server streaming Newest sensor data complete");
+//                System.out.println("Temp: " + getCurTemp() + " ; Humidity: " + getCurHumidity());
+//                System.out.println("Smart controller server streaming Newest sensor data complete");
             }
         };
 
@@ -48,7 +62,7 @@ public class UserClient {
                 .build();
 
         stub.refreshSensorData(req, resObserver);
-        System.out.println("Refresh Sensor data request sent complete");
+//        System.out.println("Refresh Sensor data request sent complete");
     }
 
     public void runEquipment(String equipName, long equipRuntime) {
@@ -56,9 +70,14 @@ public class UserClient {
             @Override
             public void onNext(UserMonitorAndControlServiceProto.EquipRunningStatusRes equipRunningStatusRes) {
                 String runningEquipName = equipRunningStatusRes.getEquipName();
-                long runningTime = equipRunningStatusRes.getEquipRunningTime();
+                long runningTimeLeft = equipRunningStatusRes.getEquipRunningTime();
 
-                System.out.println(runningEquipName + " : " + runningTime);
+                if(runningEquipName.equals("heater")){
+                    setHeaterRunningTimeLeft(runningTimeLeft);
+                }else if (runningEquipName.equals("sprinkler")){
+                    setSprinklerRunningTimeLeft(runningTimeLeft);
+                }
+
             }
 
             @Override
@@ -68,9 +87,22 @@ public class UserClient {
 
             @Override
             public void onCompleted() {
-                System.out.println("Run equip complete");
+                if(equipName.equals("heater")){
+                    setHeaterRunning(false);
+                    setHeaterRunningTimeLeft(0L);
+                }else if (equipName.equals("sprinkler")){
+                    setSprinklerRunning(false);
+                    setSprinklerRunningTimeLeft(0L);
+                }
+//                System.out.println("Run " + equipName + " complete");
             }
         };
+
+        if(equipName.equals("heater")){
+            setHeaterRunning(true);
+        }else if (equipName.equals("sprinkler")){
+            setSprinklerRunning(true);
+        }
 
         StreamObserver<UserMonitorAndControlServiceProto.RunEquipReq> reqObserver = stub.runEquipment(resObserver);
 
@@ -83,5 +115,51 @@ public class UserClient {
         reqObserver.onCompleted();
     }
 
+    public long getCurTemp() {
+        return curTemp;
+    }
 
+    public void setCurTemp(long curTemp) {
+        this.curTemp = curTemp;
+    }
+
+    public long getCurHumidity() {
+        return curHumidity;
+    }
+
+    public void setCurHumidity(long curHumidity) {
+        this.curHumidity = curHumidity;
+    }
+
+    public long getHeaterRunningTimeLeft() {
+        return heaterRunningTimeLeft;
+    }
+
+    public void setHeaterRunningTimeLeft(long heaterRunningTimeLeft) {
+        this.heaterRunningTimeLeft = heaterRunningTimeLeft;
+    }
+
+    public long getSprinklerRunningTimeLeft() {
+        return sprinklerRunningTimeLeft;
+    }
+
+    public void setSprinklerRunningTimeLeft(long sprinklerRunningTimeLeft) {
+        this.sprinklerRunningTimeLeft = sprinklerRunningTimeLeft;
+    }
+
+    public boolean isHeaterRunning() {
+        return isHeaterRunning;
+    }
+
+    public void setHeaterRunning(boolean heaterRunning) {
+        isHeaterRunning = heaterRunning;
+    }
+
+    public boolean isSprinklerRunning() {
+        return isSprinklerRunning;
+    }
+
+    public void setSprinklerRunning(boolean sprinklerRunning) {
+        isSprinklerRunning = sprinklerRunning;
+    }
 }
